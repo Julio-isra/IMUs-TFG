2fdaf8bad3f0c6ff71ac0cd7fd0bbebc908861fd
Julio Israel Peralta Lescano <peraltajulio765@gmail.com>
2026-09-14 20:17:58 +0200

    Avance actual 14/09

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.responses import FileResponse, RedirectResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 import numpy as np
 from tensorflow.keras.models import load_model
 import json
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-# --- CARGAMOS EL MODELO LSTM ---
+# --- CARGA DEL MODELO LSTM AL SERVIDOR ---
 try:
     modelo_ia = load_model('LSTM/modelo_lesiones_lstm.h5')
     print("---- Modelo LSTM cargado correctamente. ----")
@@ -17,36 +16,40 @@ except Exception as e:
     print("---- Error al cargar el modelo: ", e)
     modelo_ia = None
 
-# 1. Datos JSON
+# Datos JSON (RUATA LOCAL)
 CARPETA_DATOS = r"D:\Universidad\TFG\IMU_App_JSON"
 
-# 2. Le decimos a FastAPI que sirva la página web desde la carpeta "static"
+# ASIGNACIOÓN DE RUTA PARA EL HTML (CARPETTA: STATIC)
 app.mount("/web", StaticFiles(directory="static", html=True), name="static")
 
-# 3. Pagina principal
+# Pagina principal (RUTA)
 @app.get("/")
 def ruta_principal():
     return RedirectResponse(url="/web/")
 
-# 4. Tu tubería de datos intacta (El almacén de JSONs)
+# SOLICITUD DE DATOS DEL PACIENTE SOLICITADO (JSON)
 @app.get("/paciente/{nombre_archivo}")
 def obtener_paciente(nombre_archivo: str):
+    # VALIDACION DEL NOMBRE DEL ARCHIVO (PACIENTE)
     if not nombre_archivo.endswith('.json'):
         nombre_archivo += '.json'
-        
+    
+    # RUTA LOCAL DEL JSON DEL PACIENTE
     ruta_completa = os.path.join(CARPETA_DATOS, nombre_archivo)
     
     if not os.path.exists(ruta_completa):
         raise HTTPException(status_code=404, detail="Datos del paciente no encontrados")
     
-    # 1. Leemos el contenido del JSON
+    # LECTURA DEL CONTENIDO DEL JSON
     with open(ruta_completa, 'r') as f:
         datos_paciente = json.load(f)
     
-    # 2. Iniciamos variables de diagnóstico por defecto
+    # Iniciamos variables de diagnóstico por defecto
     diagnostico = "No evaluable"
     probabilidad = 0.0
 
+#### ¡¡¡¡¡¡¡¡¡¡ REVISAR POR QUE SE USA SOLO EL PIE IZQUIERDO !!!!!!!!!!!!  
+ 
     # 3. Procesamos con la IA si hay datos del pie izquierdo
     # Accedemos a la representación de la señal procesada sin outliers
     try:
@@ -72,7 +75,7 @@ def obtener_paciente(nombre_archivo: str):
     except KeyError:
         pass # El archivo no tiene el formato esperado para la IA
 
-    # 4. Inyectamos los resultados de la IA en los datos que enviamos a la web
+    # ENVIAMOS LOS RESULTADOS DEL MODELO AL USARIO 
     datos_paciente['ia_diagnostico'] = diagnostico
     datos_paciente['ia_probabilidad'] = probabilidad
     
